dcb04e22086c2448f96eccae38c04a5dc55bd1b7
alex <marketdoski@gmail.com>
2024-01-29 01:07:25 +0200

    added basename

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import PortfolioScreen from './screens/PortfolioScreen';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <div className='App'>      
       <main>
         <Routes>
